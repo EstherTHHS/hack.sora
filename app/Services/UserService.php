fd6831563b2e9c99f3518e9183f3026806614d0f
thethhsan <thethtoohtoosan@aceplussolutions.com>
2023-09-24 14:57:54 +0630

    add user management

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -22,6 +22,34 @@ class UserService
         return $user;
     }
 
+    public function showUser($data,$id)
+    {
+        $user = User::with('subscriptions.item')->where('id',$id)->first();
+        return $user;
+    }
+
+
+
+    public function destroy($data,$id)
+    {
+        $startTime = microtime(true);
+        $user = User::where('id',$id)->first();
+
+        if ($user->subscriptions()->count() > 0) {
+            return response()->error(request(), null, 'The Subscription  User cannot be deleted .', 400, $startTime);
+        }
+        return $user->delete();
+    }
+
+
+
+    public function updateUser($data,$id)
+    {
+        $user = User::where('id',$id)->first();
+        $user->update($data);
+        return $user;
+    }
+
 
     public function socialLogin($data)
     {
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->boolean('status')->default(1)->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
