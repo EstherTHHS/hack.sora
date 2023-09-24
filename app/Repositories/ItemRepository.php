8cd1a9d0f5770f401644483072f4576c83ab42c8
thethhsan <thethtoohtoosan@aceplussolutions.com>
2023-09-25 04:11:27 +0630

    update show item

--- app/Repositories/ItemRepository.php
+++ app/Repositories/ItemRepository.php
@@ -26,17 +26,51 @@ class ItemRepository implements ItemRepositoryInterface
 
 
 
+    // public function getItemById($id)
+    // {
+
+    //     $items= Item::where('id',$id)->get();
+    //     $items->each(function ($item) {
+    //         $item->image_url = asset('image/' . $item->image_url);
+    //     });
+    //     return $items;
+
+    // }
     public function getItemById($id)
-    {
+{
 
-        $items= Item::where('id',$id)->get();
-        $items->each(function ($item) {
-            $item->image_url = asset('image/' . $item->image_url);
-        });
+    $item = Item::find($id);
 
-        return $items;
 
+    if (!$item) {
+
+        return null;
     }
+
+
+    $item->image_url = asset('image/' . $item->image_url);
+
+
+    $category = $item->category;
+
+
+    $relatedItems = Item::where('category', $category)
+    ->inRandomOrder()
+    ->take(4)
+    ->get();
+
+
+    $relatedItems->each(function ($relatedItem) {
+        $relatedItem->image_url = asset('image/' . $relatedItem->image_url);
+    });
+
+
+    return [
+        'item' => $item,
+        'relatedItems' => $relatedItems,
+    ];
+}
+
 
     public function getItemByCategory($category)
     {
